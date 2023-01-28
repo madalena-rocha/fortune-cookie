e3383b8e6d69d02667902577cb4563d0be002f56
Madalena Machado Rocha <rochamada1997m@gmail.com>
2023-01-28 16:40:28 -0300

    update cookie messages

--- main.js
+++ main.js
@@ -7,19 +7,36 @@ const cookieMessage = document.getElementById("cookie-message");
 const openCookie = document.getElementById("open-cookie");
 
 const messages = [
-    "Pode se encontrar a felicidade mesmo nas horas mais sombrias, se a pessoa se lembrar de acender a luz.",
-    "Não vale a pena viver sonhando e se esquecer de viver.",
-    "Não são nossas habilidades que revelam quem realmente somos, são as nossas escolhas.",
-    "Em breve teremos que escolher entre o que é certo e o que é fácil.",
-    "Palavras são, na minha nada humilde opinião, nossa inesgotável fonte de magia.",
-    "Não tenha pena dos mortos. Tenha pena dos vivos, e, acima de tudo, daqueles que vivem sem amor.",
-    "Todos temos luz e trevas dentro de nós. O que importa é o lado no qual decidimos agir. Isso é o que realmente somos.",
-    "As coisas que nos pertencem acabam voltando para nós. Bom, nem sempre da forma que esperamos.",
-    "É necessário muita coragem para enfrentar nossos inimigos, e tanto quanto para enfrentar nossos amigos.",
-    "É o grau de comprometimento que determina o sucesso, não o número de seguidores.",
-    "Se você quer saber como um homem é, preste atenção em como ele trata quem é inferior a ele, não seus iguais.",
-    "Não adianta a gente ficar sentado se preocupando. O que tiver de ser será, e nós o enfrentaremos quando vier.",
-    "A verdade é uma coisa bela e terrível, e portanto deve ser tratada com grande cautela.", 
+    "A vida é feita de pequenos momentos de felicidade.",
+    "Mantenha a fé, as coisas vão dar certo.",
+    "A esperança é a última que morre.",
+    "Tudo acontece por um motivo.",
+    "A vida é curta, sorria enquanto pode.",
+    "Os desafios são oportunidades para se mostrar o que você é capaz.",
+    "A persistência é a chave do sucesso.",
+    "O sucesso é uma jornada, não um destino.",
+    "Oportunidades são escondidas dentro de cada fracasso.",
+    "O impossível é apenas uma opinião, não uma realidade.",
+    "A felicidade é uma escolha, escolha ser feliz.",
+    "Não importa o tamanho do problema, a atitude é tudo.",
+    "A gratidão é a chave para a felicidade.",
+    "O amor é a força mais poderosa do mundo.",
+    "A determinação é a chave para vencer.",
+    "A bondade é a chave para a paz interior.",
+    "A paciência é a chave para a sabedoria.",
+    "A verdadeira riqueza é a amizade e a família.",
+    "Otimismo é a chave para uma vida feliz.",
+    "O sucesso é a soma de pequenos esforços repetidos diariamente.",
+    "A honestidade é a chave para a confiança.",
+    "A generosidade é a chave para a felicidade dos outros.",
+    "A humildade é a chave para a sabedoria.",
+    "A simplicidade é a chave para a felicidade.",
+    "A confiança é a chave para o sucesso.",
+    "A coragem é a chave para superar medos e obstáculos.",
+    "A tolerância é a chave para a paz.",
+    "A empatia é a chave para o entendimento dos outros.",
+    "A sabedoria vem com a idade e a experiência.",
+    "A criatividade é a chave para o sucesso."
 ];
 
 function GenerateRandomMessage() {
@@ -36,17 +53,17 @@ function ToggleClassHide() {
 
 document.addEventListener("keypress", (e) => {
     if (e.key == 'Enter') {
-        ToggleClassHide()
+        ToggleClassHide();
         GenerateRandomMessage();
     }
 });
 
 fortuneCookie.addEventListener('click', () => {
-    ToggleClassHide()
+    ToggleClassHide();
     GenerateRandomMessage();
 });
 
 openCookie.addEventListener('click', () => {
-    ToggleClassHide()
+    ToggleClassHide();
     GenerateRandomMessage();
 });
